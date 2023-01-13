27f2e95064cce8038644e9f002d806c3cfbe6f6b
Arthur B <arthur.b@thewalkingnerds.com>
2023-01-13 14:59:30 +0100

    commit 5

--- src/views/PipiPage.vue
+++ src/views/PipiPage.vue
@@ -1,36 +1,37 @@
 <template>
-    <ion-page>
-      papa
-    </ion-page>
-  </template>
-  
-  <script setup lang="ts">
-  import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-  </script>
-  <style scoped>
-  #container {
-    text-align: center;
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 50%;
-    transform: translateY(-50%);
-  }
-  
-  #container strong {
-    font-size: 20px;
-    line-height: 26px;
-  }
-  
-  #container p {
-    font-size: 16px;
-    line-height: 22px;
-    color: #8c8c8c;
-    margin: 0;
-  }
-  
-  #container a {
-    text-decoration: none;
-  }
-  </style>
-  
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button color="primary"></ion-menu-button>
+        </ion-buttons>
+        <ion-title>Prenez une photo</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-buttons>
+        <ion-button color="primary" expand="block" fill="outline" shape="round" size="large" @click="takePicture()">
+          <ion-icon slot="start" name="camera"></ion-icon>
+          Prenez une photo
+        </ion-button>
+
+      </ion-buttons>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+
+const takePicture = () => {
+  console.log("takePicture");
+};
+</script>
